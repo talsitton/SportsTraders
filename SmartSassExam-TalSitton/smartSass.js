f67e0f6f63f5c03259cfd1b170edf0444e8f6691
tal sitton <talsitton@gmail.com>
2017-06-26 14:27:51 +0300

    some enhancements...

--- SmartSassExam-TalSitton/smartSass.js
+++ SmartSassExam-TalSitton/smartSass.js
@@ -14,36 +14,38 @@ smartSass = function (sourcePatterns, targetDirectoryRoot)
 	});
 	sassFiles.forEach( function(sassFile){
 			sassIsPartial(sassFile)? sassPartialFiles.push(sassFile): sassNotPartialFiles.push(sassFile);
-  });
+	});
 
 	sassNotPartialFiles.forEach( function(sassFile){
-			 var cssRelatedFile = path.join(targetDirectoryRoot, sassFile);
-			 if(sassIsNewOrModifyed(sassFile, cssRelatedFile) || sassPartialsModified(sassFile, sassPartialFiles, cssRelatedFile))
+			var cssRelatedFile = path.join(targetDirectoryRoot, sassFile.replace(path.extname(sassFile), ".css"));
+			if(sassIsNewOrModified(sassFile, cssRelatedFile) || sassPartialsModified(sassFile, sassPartialFiles, cssRelatedFile))
 			 		 filesWhichNeedSass.push(sassFile);
 	});
  	return filesWhichNeedSass;
 }
 
-sassIsPartial = function(sassFile){
+sassIsPartial = function(sassFile){	
 	return (path.basename(sassFile).charAt(0) =='_');
 }
 
-sassIsNewOrModifyed = function(sFile, relatedCssFile){
+sassIsNewOrModified = function(sFile, relatedCssFile){
 	return (!fs.existsSync(relatedCssFile) || fs.statSync(sFile).mtime > fs.statSync(relatedCssFile).mtime);
 }
 
 sassPartialsModified = function(sFile, sPartialFiles, relatedCssFile){
-	var partials = [];
+	
+	var importedPartials = [];
 	var regexp = /@import\s+[^\s\\]+/g;				// regular expression to find partials
-	var importFound = fs.readFileSync(sFile).toString().match(regexp);
-	if(importFound == null) return false;
-	importFound.forEach(function(match){
-			partials.push( "_" + path.basename(match.split(" ").pop()));
+	var importsFound = fs.readFileSync(sFile).toString().match(regexp);
+	if(importsFound == null) return false;			// no imported partials sass files found
+	
+	importsFound.forEach(function(match){
+			importedPartials.push( "_" + path.basename(match.split(" ").pop()));
 	});
-
-	for(var partial in partials){
-		for(var spFile in sPartialFiles){
-			if(spFile.indexOf(partials[partial]) >= 0 && sassIsNewOrModifyed(sPartialFiles[spFile], relatedCssFile)){
+	
+	for(var i=0; i< importedPartials.length; i++){
+		for(var j=0; j< sPartialFiles.length; j++){
+			if(sPartialFiles[j].indexOf(importedPartials[j]) >=0 && sassIsNewOrModified(sPartialFiles[j], relatedCssFile)){
 				return true;
 			}
 		}
